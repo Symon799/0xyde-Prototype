c2716bdbb9aad0deab5d81ae49ea478fe08a20eb
Symon799 <simon.rigard@epita.fr>
2014-11-04 00:03:01 +0100

    Initial Commit

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet : MonoBehaviour {
+
+	void OnCollisionEnter(Collision c)
+	{
+		rigidbody.isKinematic = true;
+		renderer.enabled = false;
+		Destroy (gameObject, 0.3f);
+	}
+}
