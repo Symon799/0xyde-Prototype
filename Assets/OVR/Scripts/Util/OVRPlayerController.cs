f80c6430536cb7e3d6519c5200865fd6cec6f1f8
Symon799 <simon.rigard@epita.fr>
2014-11-06 18:42:33 +0100

    Changed position of the Hand & Gun in Simple3D mode Added Jump in oculus mode

--- Assets/OVR/Scripts/Util/OVRPlayerController.cs
+++ Assets/OVR/Scripts/Util/OVRPlayerController.cs
@@ -46,7 +46,7 @@ public class OVRPlayerController : MonoBehaviour
 	/// <summary>
 	/// The force applied to the character when jumping.
 	/// </summary>
-	public float JumpForce = 0.3f;
+	private float JumpForce = 0.2f;
 
 	/// <summary>
 	/// The rate of rotation when using a gamepad.
@@ -174,6 +174,11 @@ public class OVRPlayerController : MonoBehaviour
 
 	public virtual void UpdateMovement()
 	{
+		if (Input.GetButtonDown("Jump"))
+		{
+			Jump();
+		}
+
 		if (HaltUpdateMovement)
 			return;
 
@@ -203,8 +208,8 @@ public class OVRPlayerController : MonoBehaviour
 			MoveScale = 0.70710678f;
 
 		// No positional movement if we are in the air
-		if (!Controller.isGrounded)
-			MoveScale = 0.0f;
+		//if (!Controller.isGrounded)
+		//	MoveScale = 0.0f;
 
 		MoveScale *= SimulationRate * Time.deltaTime;
 
