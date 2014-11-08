5f1670948084e1a155089e2b8b780fc1e8721262
sybiload <sybiload@gmail.com>
2014-11-08 22:25:21 +0100

    Added zombie prefabs, zombie animations, mAI scripts, NavMeshAgent

---
+++ Assets/Scripts/mAI.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class mAI : MonoBehaviour {
+
+	public Transform leader;
+	public float maxDistance = 7;
+	public float minDistance = 2;
+	private Animator anim;
+
+	// Use this for initialization
+	void Start () 
+	{
+		anim = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		// Freeze Y axis
+		transform.eulerAngles = new Vector3(0, transform.eulerAngles.y, 0);
+
+		AI ();
+	}
+
+	void AI () 
+	{
+		if (Vector3.Distance (transform.position, leader.position) >= minDistance) 
+		{
+			GetComponent<NavMeshAgent> ().destination = leader.position;
+			anim.Play ("walk03");
+		}
+		else
+		{
+			GetComponent<NavMeshAgent> ().ResetPath();
+			attack();
+		}
+	}
+
+	void attack()
+	{
+		anim.Play ("atack01");
+	}
+}
