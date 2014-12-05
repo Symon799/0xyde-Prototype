7510cce30ba31887309006d6fd618fddc49ec169
sybiload <sybiload@gmail.com>
2014-12-05 22:37:36 +0100

    Changed animator, added a die feature to zombies, solved the broken animation bug

--- Assets/Scripts/mAI.cs
+++ Assets/Scripts/mAI.cs
@@ -4,9 +4,11 @@ using System.Collections;
 public class mAI : MonoBehaviour {
 
 	public Transform leader;
-	public float maxDistance = 7;
+	public float maxDistance = 8;
 	public float minDistance = 2;
 	private Animator anim;
+
+	private int life = 1;
 
 	// Use this for initialization
 	void Start () 
@@ -23,22 +25,96 @@ public class mAI : MonoBehaviour {
 		AI ();
 	}
 
-	void AI () 
+	private void AI () 
 	{
-		if (Vector3.Distance (transform.position, leader.position) >= minDistance) 
+		if (life > 0) 
 		{
-			GetComponent<NavMeshAgent> ().destination = leader.position;
-			anim.Play ("walk03");
-		}
-		else
-		{
-			GetComponent<NavMeshAgent> ().ResetPath();
-			attack();
+			if (Vector3.Distance (transform.position, leader.position) >= minDistance) 
+			{
+				walk();
+			}
+			else
+			{
+				attack();
+			}
 		}
 	}
 
-	void attack()
+	/// <summary>
+	/// Walk behaviour function
+	/// </summary>
+	private void walk()
 	{
-		anim.Play ("atack01");
+		// Initiate NavMesh agent
+		GetComponent<NavMeshAgent> ().destination = leader.position;
+
+		// Start walk motion
+		anim.SetBool ("attack", false);
+		anim.SetBool ("walk", true);
+
+		Debug.Log("Walk");
+	}
+
+	/// <summary>
+	/// Attack behaviour function
+	/// </summary>
+	private void attack()
+	{
+		// Stop NavMesh agent
+		GetComponent<NavMeshAgent> ().ResetPath();
+
+		// Start attack motion
+		anim.SetBool ("attack", true);
+
+		Debug.Log("Attack");
+	}
+
+	/// <summary>
+	/// Remove 1 point of life to the zombie
+	/// </summary>
+	public void hurt()
+	{
+		life--;
+
+		if (life <= 0) 
+		{
+			die ();
+		}
+		else
+		{
+			Debug.Log("Hurt");
+		}
+	}
+
+	/// <summary>
+	/// Remove the given point of live in parameter
+	/// </summary>
+	/// <param name="hurtPoint">Hurt point.</param>
+	public void hurt(int hurtPoint)
+	{
+		life -= hurtPoint;
+
+		if (life <= 0) 
+		{
+			die ();
+		}
+		else
+		{
+			Debug.Log("Hurt");
+		}
+	}
+
+	/// <summary>
+	/// Kill the zombie
+	/// </summary>
+	private void die()
+	{
+		// Stop NavMesh agent (unless you want self-moving bodies..)
+		GetComponent<NavMeshAgent> ().ResetPath();
+
+		// Start dead motion
+		anim.SetBool ("alive", false);
+
+		Debug.Log("You've been targeted for termination");
 	}
 }
